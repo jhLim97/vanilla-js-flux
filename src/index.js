caa3cc094cb81815c7b5846745f1051d6413b113
jhLim97 <wnsgur2749@naver.com>
2021-12-28 18:08:28 +0900

    chore: 모든 브라우저에서 호환되는 문법으로 변환하기 위한 Babel 설정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import { sum } from './math';
 
 function component() {
   var element = document.createElement('div');
 
   /* lodash is required for the next line to work */
   element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.innerHTML += `Babel test => 10 + 20 = ${sum(10, 20)}`;
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'none',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         clean: true,
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+        ]
+    }, 
 }
