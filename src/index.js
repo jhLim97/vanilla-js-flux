24c6d43b565e2743af894fd19184a693ec402fac
jhLim97 <wnsgur2749@naver.com>
2021-12-28 19:11:42 +0900

    style: 기존 파일들에 eslint, prettier 설정 적용

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import heartImage from './assets/heart.png';
 import { sum } from './math';
 
 function component() {
-  const element = document.createElement('div');
-  const imageElement = document.createElement('img');
+    const element = document.createElement('div');
+    const imageElement = document.createElement('img');
 
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-  element.innerHTML += `Babel test => 10 + 20 = ${sum(10, 20)}`;
+    /* lodash is required for the next line to work */
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML += `Babel test => 10 + 20 = ${sum(10, 20)}`;
 
-  imageElement.src = heartImage;
-  element.appendChild(imageElement);
+    imageElement.src = heartImage;
+    element.appendChild(imageElement);
 
-  return element;
+    return element;
 }
 
-const root = document.querySelector("#root");
+const root = document.querySelector('#root');
 root.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -15,29 +15,25 @@ module.exports = {
             {
                 test: /\.js$/,
                 use: 'babel-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
-                ],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/,
                 type: 'asset/resource',
             },
-        ]
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
         }),
     ],
     devServer: {
         port: 9000,
         historyApiFallback: true,
     },
-}
+};
