bd46f97c4b6079ee77513050e83be8f2f8c272cf
jhLim97 <wnsgur2749@naver.com>
2022-01-16 21:40:31 +0900

    feat: 홈, test 페이지 라우팅 설정

--- src/app.js
+++ src/app.js
@@ -1,21 +1,7 @@
-import _ from 'lodash';
-import './common.scss';
-import heartImage from './assets/heart.png';
-import { sum } from './math';
+import Router from './router';
 
-function component() {
-    const element = document.createElement('div');
-    const imageElement = document.createElement('img');
+const router = () => {
+    Router.init();
+};
 
-    /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.innerHTML += `Babel test => 10 + 20 = ${sum(10, 20)}`;
-
-    imageElement.src = heartImage;
-    element.appendChild(imageElement);
-
-    return element;
-}
-
-const root = document.querySelector('#root');
-root.appendChild(component());
+window.addEventListener('DOMContentLoaded', router);
