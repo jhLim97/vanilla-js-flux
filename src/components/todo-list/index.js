51d37fd2bae2c73a3a1f2d0680b46b7eb4653b8d
jhLim97 <wnsgur2749@naver.com>
2022-01-17 00:52:10 +0900

    feat: todo 항목 삭제로직 구현(by flux)
    * flux 패턴 기반으로 특정 todo 삭제 시 관련된 뷰들이 자동으로 리렌더링
      되도록 구현했습니다.

--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,16 +1,36 @@
 import Component from '../../core/Component';
-import { todoStore } from '../../stores/todoList';
+import { todoStore, DELETE_ACTION } from '../../stores/todoList';
 
 export default class TodoList extends Component {
     template() {
         const { todos } = todoStore.getState();
         const template = `
             <div class="home-main__todos">
-                ${todos.map((todo) => `<div>${todo}</div>`).join('')}
+                ${todos
+                    .map(
+                        (todo, id) =>
+                            `<div class="home-main__todo">
+                                ${todo}
+                                <button data-id="${id}">삭제</button>
+                            </div>`,
+                    )
+                    .join('')}
             </div>
         `;
 
         return template;
+    }
+
+    setEvent() {
+        const deleteTodo = (e) => {
+            const deleteButton = e.target.closest('button');
+            if (!deleteButton) return;
+
+            const targetId = Number(deleteButton.dataset.id);
+            todoStore.dispatch(DELETE_ACTION(targetId));
+        };
+
+        this.target.addEventListener('click', deleteTodo);
     }
 
     didMount() {
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -5,7 +5,7 @@ const todoReducer = (state = {}, action) => {
         case 'ADD':
             return { todos: [...state.todos, action.todo] };
         case 'DELETE':
-            return { todos: [...state.todos.filter((todo) => todo.id !== action.id)] };
+            return { todos: [...state.todos.filter((todo, id) => id !== action.id)] };
         default:
             return state;
     }
@@ -14,3 +14,4 @@ const todoReducer = (state = {}, action) => {
 const initState = { todos: ['백준 1문제 풀기', '‘그해 우리는’ 시청', '밥 맛있게 먹기'] };
 export const todoStore = CreateStore(initState, todoReducer);
 export const ADD_ACTION = (todo) => ({ type: 'ADD', todo });
+export const DELETE_ACTION = (id) => ({ type: 'DELETE', id });
